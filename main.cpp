7270dded1fd691884463ebaf4ec1b3ce537e1f05
Javier Ixcolin <javierixcolin202132163@cunoc.edu.gt>
2025-03-22 00:27:15 -0600

    Iniciando repositorio

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+using namespace std;
+
+int main()
+{
+
+    bool optionIsCorrect = false;
+    int option = 0;
+
+    do
+    {
+        cout << "---------- BIENVENIDO AL JUEGO DEL LABERINTO ----------" << endl;
+        cout << "Menu de opciones:" << endl;
+        cout << "1. Jugar" << endl;
+        cout << "2. Historial de partidas" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Ingrese la opcion deseada: ";
+
+        try
+        {
+            cin >> option;
+            if (cin.fail())
+            {
+                throw runtime_error(" Tipo de dato incorrecto\n");
+            }
+        }
+        catch (const std::exception &e)
+        {
+            system("clear");
+            cout << "ERROR." << e.what() << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        switch (option)
+        {
+        case 1:
+            cout << "Iniciando el juego..." << endl;
+            /* code */
+            break;
+        case 2:
+            cout << "Historial de partidas" << endl;
+            /* code */
+            break;
+        case 3:
+            cout << "Saliendo del juego..." << endl;
+            optionIsCorrect = true;
+            system("clear");
+            break;
+        default:
+            system("clear");
+            cout << "ERROR. Opcion no valida" << endl;
+            break;
+        }
+
+    } while (!optionIsCorrect);
+
+    return 0;
+}
